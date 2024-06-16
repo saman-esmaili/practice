3583492a3841f6fbd243f8cae5f52ec62bc7f558
saman <saman.esmaili204@gmail.com>
2024-06-16 17:16:50 +0330

    fix form and complete password with 3 characters

--- password.py
+++ password.py
@@ -1,18 +1,18 @@
 import string
 import tkinter as tk
+import random
 
-lowercase = [string.ascii_lowercase]
-uppercase = [string.ascii_uppercase]
 
 class PasswordGenerator(tk.Tk):
     def __init__(self):
         super().__init__()
         self.geometry("380x250")
-        self.resizable(width=False,height=False)
+        self.resizable(width=False, height=False)
         self.title("password generator")
         self.configure(background="#34495E")
         self.center(self)
         self.controller()
+        self.generate()
     def center(self, win):
         win.update_idletasks()
         width = win.winfo_width()
@@ -27,25 +27,57 @@ class PasswordGenerator(tk.Tk):
         win.deiconify()
 
     def controller(self):
-        # mainFrame = tk.Frame(self,background="#34495E")
-        # mainFrame.pack(side="top",anchor="e")
-        tk.Label(self,background="#34495E",fg="#EDE3F6",text="choose number of characters for your password").grid(row=0,column=1,padx=3,pady=3,ipadx=3,ipady=3)
+        tk.Label(self, background="#34495E", fg="#EDE3F6", text="choose number of characters for your password").grid(
+            row=0, column=1, padx=3, pady=3, ipadx=3, ipady=3)
         self.numbers = tk.IntVar()
         self.numbers.get()
-        self.radio1 = tk.Radiobutton(self,text="3",background="#34495E",fg="#EDE3F6",variable=self.numbers,value="1",selectcolor="#34495E")
-        self.radio2 = tk.Radiobutton(self,text="5",background="#34495E",fg="#EDE3F6",variable=self.numbers,value="2",selectcolor="#34495E")
-        self.radio3 = tk.Radiobutton(self,text="14",background="#34495E",fg="#EDE3F6",variable=self.numbers,value="3",selectcolor="#34495E")
-        self.radio1.grid(row=1,column=2,padx=3,pady=3,ipadx=3,ipady=3)
-        self.radio2.grid(row=1,column=1,padx=3,pady=3,ipadx=3,ipady=3)
-        self.radio3.grid(row=1,column=0,padx=3,pady=3,ipadx=3,ipady=3)
-        tk.Button(self,text="Generate",command=self.show,background="#9585A3",fg="#1C0532").grid(row=2,column=1,padx=3,pady=3,ipadx=3,ipady=3)
+        self.option3 = tk.Radiobutton(self, text="3", background="#34495E", fg="#EDE3F6", variable=self.numbers,
+                                      value=1, selectcolor="#34495E",activebackground="#34495E")
+        self.option5 = tk.Radiobutton(self, text="5", background="#34495E", fg="#EDE3F6", variable=self.numbers,
+                                      value=2, selectcolor="#34495E",activebackground="#34495E")
+        self.option14 = tk.Radiobutton(self, text="14", background="#34495E", fg="#EDE3F6", variable=self.numbers,
+                                       value=3, selectcolor="#34495E",activebackground="#34495E")
+        self.option3.grid(row=1, column=2, padx=3, pady=3, ipadx=3, ipady=3)
+        self.option5.grid(row=1, column=1, padx=3, pady=3, ipadx=3, ipady=3)
+        self.option14.grid(row=1, column=0, padx=3, pady=3, ipadx=3, ipady=3)
+
+        tk.Button(self, text="Generate", command=self.show, background="#9585A3", fg="#1C0532",activebackground="#34495E").grid(row=2, column=1,
+                                                                                                     padx=3, pady=3,
+                                                                                                     ipadx=3, ipady=3)
         self.result = tk.StringVar()
-        tk.Entry(self,textvariable=self.result,justify="center",state="readonly").grid(row=3,column=1,padx=3,pady=15,ipadx=3,ipady=3)
+        tk.Entry(self, textvariable=self.result, justify="center", state="readonly").grid(row=3, column=1, padx=3,
+                                                                                          pady=15, ipadx=3, ipady=3)
+        tk.Button(self,text="copy",command=self.copy, background="#9585A3", fg="#1C0532",activebackground="#34495E").grid(row=4, column=1, padx=3,pady=3, ipadx=3, ipady=3)
     def generate(self):
-        pass
+        lists = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't',
+                 'u', 'v', 'w', 'x', 'y', 'z'
+            , 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U',
+                 'V', 'W', 'X', 'Y', 'Z'
+            , '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+        self.pass_list = []
+        word = ''
+        for i in range(0,62):
+            word += lists[i]
+            for i2 in range(0,62):
+                word = word[0:1]
+                word += lists[i2]
+                for i3 in range(0,62):
+                    word += lists[i3]
+                    self.pass_list.append(word)
+                    word = word[0:2]
+            word = ''
+        print(len(self.pass_list))
 
     def show(self):
-        pass
+        chosen = self.numbers.get()
+        if chosen == 1:
+            index = random.randint(0,len(self.pass_list))
+            self.result.set(self.pass_list[index])
+
+    def copy(self):
+        value = self.result.get()
+        self.clipboard_clear()
+        self.clipboard_append(value)
 
 if __name__ == "__main__":
     app = PasswordGenerator()
