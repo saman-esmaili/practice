04b07c711c8b7d11228d6a6d35e0386f6693c96b
saman <saman.esmaili204@gmail.com>
2024-06-18 17:30:22 +0330

    delete option2

--- tenth-min.py
+++ tenth-min.py
@@ -4,15 +4,9 @@ for i in range(amount):
     number = float(input("enter number:"))
     numbers.append(number)
 
-# option1
 for i in range(9):
     min1 = min(numbers)
     numbers.remove(min1)
 final_min = min((numbers))
 
 print(final_min)
-
-# option2
-
-sortNum = sorted(numbers)
-print(sortNum[9])
