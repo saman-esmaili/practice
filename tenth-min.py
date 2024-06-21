7ee28f5627d5bd844b842e4e98dcf91792484f80
saman <saman.esmaili204@gmail.com>
2024-06-21 18:45:13 +0330

    solved in another way

--- tenth-min.py
+++ tenth-min.py
@@ -3,10 +3,18 @@ numbers = []
 for i in range(amount):
     number = float(input("enter number:"))
     numbers.append(number)
+#
+# for i in range(9):
+#     min1 = min(numbers)
+#     numbers.remove(min1)
+# final_min = min((numbers))
+#
+# print(final_min)
 
-for i in range(9):
-    min1 = min(numbers)
-    numbers.remove(min1)
-final_min = min((numbers))
-
-print(final_min)
+for i in range(len(numbers)):
+    counter = 0
+    for index in range(len(numbers)):
+        if numbers[i] > numbers[index]:
+            counter += 1
+    if counter == 9:
+        print(numbers[i])
