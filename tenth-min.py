6f8b6a11741560c1c72da81e7d2ccc3a0ff644ad
saman <saman.esmaili204@gmail.com>
2024-06-27 13:19:51 +0330

    solved with one for

--- tenth-min.py
+++ tenth-min.py
@@ -11,10 +11,24 @@ for i in range(amount):
 #
 # print(final_min)
 
-for i in range(len(numbers)):
-    counter = 0
-    for index in range(len(numbers)):
-        if numbers[i] > numbers[index]:
-            counter += 1
-    if counter == 9:
-        print(numbers[i])
+# for i in range(len(numbers)):
+#     counter = 0
+#     for index in range(len(numbers)):
+#         if numbers[i] > numbers[index]:
+#             counter += 1
+#     if counter == 9:
+#         print(numbers[i])
+
+index = 0
+index2 = 0
+counter = 0
+for i in range(len(numbers)*len(numbers)):
+    if i % len(numbers) == 0 and i != 0:
+        if counter == 9:
+            print(numbers[index])
+        index += 1
+        index2 = 0
+        counter = 0
+    if numbers[index] > numbers[index2]:
+        counter += 1
+    index2 += 1
