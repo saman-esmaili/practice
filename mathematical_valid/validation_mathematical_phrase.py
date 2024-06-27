dae40246ce00843dbb6ab5deeee401b7faf7beb8
saman <saman.esmaili204@gmail.com>
2024-06-27 15:09:54 +0330

    solved with stack

--- mathematical_valid/validation_mathematical_phrase.py
+++ mathematical_valid/validation_mathematical_phrase.py
@@ -9,7 +9,8 @@ class Validation(tk.Tk):
         self.title("validation")
         self.parentheses = []
         self.operators = []
-        self.top_index=-1
+        self.parentheses_index=-1
+        self.operators_index=-1
         self.controller()
         self.center(self)
 
@@ -36,8 +37,48 @@ class Validation(tk.Tk):
         self.result = tk.StringVar()
         tk.Label(frame,textvariable=self.result,background="#57A49A").grid(row=2,column=0,padx=3,pady=3,ipadx=3,ipady=3)
 
+    def push(self):
+        phrase = self.phrase.get()
+        if phrase[0] != '-':
+            self.final_phrase = f"+{phrase}"
+        else:
+            self.final_phrase = phrase
+        for phs in self.final_phrase:
+            if phs == "(":
+                self.parentheses.append(phs)
+                self.parentheses_index += 1
+            elif phs == "+" or phs == "-":
+                self.operators.append(phs)
+                self.operators_index += 1
+
+    def pop(self):
+        index = 0
+        for phs in self.final_phrase:
+            if phs == ")":
+                if self.operators_index != -1:
+                    self.operators_index = -2
+                else:
+                    if self.parentheses_index >= 0:
+                        self.parentheses.pop()
+                        self.parentheses_index -= 1
+                    else:
+                        self.parentheses_index = -2
+            elif phs != "(" and phs != "+" and phs != "-":
+                if self.operators_index >= 0:
+                    self.operators.pop()
+                    self.operators_index -= 1
+
     def valid(self):
-        pass
+        phrase = self.phrase.get()
+        if phrase[0] == ")" or phrase[len(phrase)-1] == "(":
+            self.result.set("invalid")
+        else:
+            self.push()
+            self.pop()
+            if self.parentheses_index == -1 and self.operators_index == -1:
+                self.result.set("valid")
+            else:
+                self.result.set("invalid")
 
 
 if __name__ == "__main__":
