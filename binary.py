086bd1a4d6fbc177f535fb2ca6d5a1ab20452545
saman <saman.esmaili204@gmail.com>
2024-07-04 15:31:39 +0330

    convert decimal to binary

---
+++ binary.py
@@ -0,0 +1,18 @@
+class Binary():
+    def __init__(self,number,base):
+        self.number = number
+        self.base =base
+
+    def convert_binary(self):
+        self.binaries = []
+        while self.number // self.base != 0:
+            result = self.number % self.base
+            self.binaries.append(result)
+            self.number = self.number // self.base
+        self.binaries.append(self.number)
+        return self.binaries[::-1]
+
+
+binary = Binary(20,4)
+print(binary.convert_binary())
+
