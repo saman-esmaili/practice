e1d0fb5a6f194975567b026ccef68b816615032b
saman <saman.esmaili204@gmail.com>
2024-07-14 10:49:29 +0330

    solved with better way

--- graph/graphs.py
+++ graph/graphs.py
@@ -14,8 +14,9 @@ class Generator():
         ])
         graph = nx.from_numpy_array(self.matrix,create_using=nx.DiGraph)
 
-        self.lables = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G"}
-        graph = nx.relabel_nodes(graph,self.lables)
+        self.show_lables = {0:"A",1:"B",2:"C",3:"D",4:"E",5:"F",6:"G"}
+        self.lables = {"A":0,"B":1,"C":2,"D":3,"E":4,"F":5,"G":6}
+        graph = nx.relabel_nodes(graph,self.show_lables)
 
         nx.draw(graph,with_labels=True,node_color="lightblue",edge_color="gray",arrows=True)
         plt.show()
